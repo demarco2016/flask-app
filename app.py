17b06422e5a1543b177206fb8596f4d18aca9fbb
demarco2016 <demarco2016@gmail.com>
2026-06-27 12:39:34 +0200

    Add base:app_id meta tag

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
-
 app = Flask(__name__)
-
 @app.route("/")
 def home():
-    return "<h1>🚀 Flask App by demarco2016</h1><p>Web3 Builder | Arc Ecosystem | Base Chain</p>"
-
+    return """
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="base:app_id" content="6a0b13de7abfff0aca7b175a" />
+</head>
+<body>
+    <h1>🚀 Flask App by demarco2016</h1>
+    <p>Web3 Builder | Arc Ecosystem | Base Chain</p>
+</body>
+</html>
+"""
 if __name__ == "__main__":
     app.run(debug=True)
