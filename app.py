13df335545bb528761794797cf0e67a413605df0
demarco2016 <36988556+demarco2016@users.noreply.github.com>
2026-07-02 15:08:31 +0200

    Register invoices blueprint in app.py
    Added Flask blueprint for invoices and updated app structure.

--- app.py
+++ app.py
@@ -8,4 +8,16 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
+from flask import Flask, render_template
+from routes.invoices import invoices_bp  # 👈 زيد هادي
+
+app = Flask(__name__)
+
+app.register_blueprint(invoices_bp)  # 👈 وزيد هادي
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)    
